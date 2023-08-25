483809f49edc49d3d51970ed715fd80e8f77a180
K-Pomian <pomian.konrad@gmail.com>
2023-08-25 15:33:33 +0200

    Add AskKey and BidKey

--- matching-engine/src/structs/orderbook.rs
+++ matching-engine/src/structs/orderbook.rs
@@ -1,8 +1,54 @@
 use std::collections::BTreeMap;
 
+use rust_decimal::Decimal;
+
 use super::order::LimitOrder;
 
 pub struct Orderbook {
-    pub asks: BTreeMap<u64, LimitOrder>,
-    pub bids: BTreeMap<u64, LimitOrder>,
+    pub asks: BTreeMap<AskKey, LimitOrder>,
+    pub bids: BTreeMap<BidKey, LimitOrder>,
+}
+
+#[derive(Eq, PartialEq, Clone, Copy)]
+pub struct AskKey {
+    pub price: Decimal,
+    pub order_id: u64,
+}
+
+impl Ord for AskKey {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        if self.price == other.price {
+            self.order_id.cmp(&other.order_id)
+        } else {
+            self.price.cmp(&other.price)
+        }
+    }
+}
+
+impl PartialOrd for AskKey {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[derive(Eq, PartialEq, Clone, Copy)]
+pub struct BidKey {
+    pub price: Decimal,
+    pub order_id: u64,
+}
+
+impl Ord for BidKey {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        if self.price == other.price {
+            self.order_id.cmp(&other.order_id)
+        } else {
+            self.price.cmp(&other.price).reverse()
+        }
+    }
+}
+
+impl PartialOrd for BidKey {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
 }
